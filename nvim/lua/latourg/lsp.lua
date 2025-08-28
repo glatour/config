662a791d0f5100804e629cc08701cf16cdcdb39a
Guillaume Latour <glatour@genetec.com>
2025-08-28 17:06:04 -0400

    config update'

---
+++ nvim/lua/latourg/lsp.lua
@@ -0,0 +1,226 @@
+-- local servers = {
+--   omnisharp = {
+--     cmd = { 'dotnet', vim.fs.normalize(vim.fn.stdpath 'data' .. '/mason/packages/omnisharp/libexec/OmniSharp.dll') },
+--     root_dir = function(fname)
+--       local primary = require('lspconfig').util.root_pattern '*.sln'(fname)
+--       local fallback = require('lspconfig').util.root_pattern '*.csproj'(fname)
+--       return primary or fallback
+--     end,
+--     settings = {
+--       MSBuild = {
+--         LoadProjectOnDemand = true,
+--       },
+--       RoslynExtensionsOptions = {
+--         EnableAnalyzersSupport = true,
+--         EnableImportCompletion = true,
+--         EnableDecompilationSupport = true,
+--         inlayHintsOptions = {
+--           enableForParameters = true,
+--           forLiteralParameters = true,
+--           forIndexerParameters = true,
+--           forObjectCreationParameters = true,
+--           forOtherParameters = true,
+--           suppressForParametersThatDifferOnlyBySuffix = false,
+--           suppressForParametersThatMatchMethodIntent = false,
+--           suppressForParametersThatMatchArgumentName = false,
+--           enableForTypes = true,
+--           forImplicitVariableTypes = true,
+--           forLambdaParameterTypes = true,
+--           forImplicitObjectCreation = true,
+--         },
+--       },
+--       FormattingOptions = {
+--         EnableEditorConfigSupport = true,
+--         OrganizeImports = true,
+--       },
+--       Sdk = {
+--         IncludePrereleases = false,
+--       },
+--     },
+--   },
+--   ts_ls = {},
+--   terraformls = {},
+--   lua_ls = {
+--     settings = {
+--       Lua = {
+--         completion = {
+--           callSnippet = 'Replace',
+--         },
+--         diagnostics = {
+--           globals = { 'vim' },
+--           -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+--           disable = { 'missing-fields' },
+--         },
+--         hint = {
+--           enable = true,
+--         },
+--       },
+--     },
+--   },
+-- }
+--
+-- local ensure_installed = vim.tbl_keys(servers or {})
+-- vim.list_extend(ensure_installed, {
+--   'stylua',
+--   -- "csharpier",
+--   'jq',
+--   'markdownlint',
+-- })
+--
+-- ---On lsp attach setup LSP specific key bindings
+-- ---@param client table
+-- ---@param bufnr number
+-- local on_attach = function(client, bufnr)
+--   local map = function(keys, func, desc, mode)
+--     mode = mode or 'n'
+--     vim.keymap.set(mode, keys, func, { buffer = bufnr, noremap = true, desc = 'LSP: ${desc}' % { desc = desc } })
+--   end
+--
+--   -- local function open_selection_split(prompt_bufnr)
+--   --     local actions = require("telescope.actions")
+--   --     actions.select_default:replace(function()
+--   --         local selection = require("telescope.actions.state").get_selected_entry()
+--   --         if selection == nil then
+--   --             return
+--   --         end
+--   --         actions.close(prompt_bufnr)
+--   --         vim.cmd(string.format([[vsplit %s]], selection.value))
+--   --     end)
+--   --     return true
+--   -- end
+--
+--   if client.name == 'omnisharp' then
+--     require('fidget.notification').notify 'Omnisharp is attaching...'
+--
+--     local getIvyTheme = function(opts)
+--       opts = opts or {}
+--       opts['winblend'] = 10
+--       opts['preview_title'] = false
+--       opts['results_title'] = false
+--       return require('telescope.themes').get_ivy(opts)
+--     end
+--
+--     map('gd', function()
+--       require('omnisharp_extended').telescope_lsp_definition(getIvyTheme())
+--     end, '[G]oto [D]efinition')
+--     map('gr', function()
+--       require('omnisharp_extended').telescope_lsp_references(getIvyTheme { excludeDefinition = true })
+--     end, '[G]oto [R]eferences')
+--     map('gI', function()
+--       require('omnisharp_extended').telescope_lsp_implementation(getIvyTheme())
+--     end, '[G]oto [I]mplementation')
+--     map('<leader>D', function()
+--       require('omnisharp_extended').telescope_lsp_type_definition(getIvyTheme())
+--     end, 'Type [D]efinition')
+--   else
+--     map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+--     map('gr', function()
+--       require('telescope.builtin').lsp_references { include_declaration = false }
+--     end, '[G]oto [R]eferences')
+--     map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+--     -- Jump to the type of the word under your cursor.
+--     --  Useful when you're not sure what type a variable is and you want to see
+--     --  the definition of its *type*, not where it was *defined*.
+--     map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+--   end
+--
+--   -- Fuzzy find all the symbols in your current document.
+--   --  Symbols are things like variables, functions, types, etc.
+--   map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+--
+--   -- Fuzzy find all the symbols in your current workspace.
+--   --  Similar to document symbols, except searches over your entire project.
+--   map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+--
+--   -- Rename the variable under your cursor.
+--   --  Most Language Servers support renaming across files, etc.
+--   map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+--
+--   -- Execute a code action, usually your cursor needs to be on top of an error
+--   -- or a suggestion from your LSP for this to activate.
+--   map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+--
+--   map(']e', function()
+--     vim.diagnostic.goto_next { severity = vim.diagnostic.severity.ERROR }
+--   end, 'Next Diagnostics [E]rror')
+--   map('[e', function()
+--     vim.diagnostic.goto_prev { severity = vim.diagnostic.severity.ERROR }
+--   end, 'Previous Diagnostics [E]rror')
+--   map('<leader>K', vim.diagnostic.open_float, 'Diagnostic hover')
+--
+--   -- WARN: This is not Goto Definition, this is Goto Declaration.
+--   --  For example, in C this would take you to the header.
+--   map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+--
+--   local attachingTo = vim.api.nvim_buf_get_name(bufnr)
+--
+--   if
+--     client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
+--     and not (Globals.Starts(attachingTo, '/$metadata$/') and vim.bo[bufnr].filetype == 'cs')
+--   then
+--     require('fidget.notification').notify 'Highlight supported'
+--     local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+--     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+--       buffer = bufnr,
+--       group = highlight_augroup,
+--       callback = vim.lsp.buf.document_highlight,
+--     })
+--
+--     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+--       buffer = bufnr,
+--       group = highlight_augroup,
+--       callback = vim.lsp.buf.clear_references,
+--     })
+--
+--     vim.api.nvim_create_autocmd('LspDetach', {
+--       group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+--       callback = function(event)
+--         vim.lsp.buf.clear_references()
+--         vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event.buf }
+--       end,
+--     })
+--   else
+--     require('fidget.notification').notify 'Lsp highlight is not supported for this file'
+--   end
+--
+--   if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint or client.name == 'omnisharp') then
+--     map('<leader>ht', function()
+--       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
+--     end, '[T]oggle Inlay [H]ints')
+--   end
+-- end
+--
+-- -- LSP servers and clients are able to communicate to each other what features they support.
+-- --  By default, Neovim doesn't support everything that is in the LSP specification.
+-- --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+-- --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- -- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+-- capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
+--
+-- -- NOTE: The order matters: require("mason") -> require("mason-tool-installer") -> require("mason-lspconfig") -> require("lspconfig")
+-- require('mason').setup {
+--   ui = {
+--     icons = {
+--       package_installed = '✓',
+--       package_pending = '➜',
+--       package_uninstalled = '✗',
+--     },
+--   },
+-- }
+--
+-- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+--
+-- require('mason-lspconfig').setup {
+--   handlers = {
+--     function(server_name)
+--       local server = servers[server_name] or {}
+--       server.on_attach = on_attach
+--
+--       -- passing in the capabilities merges with the blink ones
+--       server.capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+--
+--       require('lspconfig')[server_name].setup(server)
+--     end,
+--   },
+-- }
