57072731e8fae51ec92a3d775418980c0baf84cd
glatour <guillaume.latour@gmail.com>
2025-11-15 21:33:41 -0500

    fixes

--- nvim/lua/latourg/plugins/autocompletion.lua
+++ nvim/lua/latourg/plugins/autocompletion.lua
@@ -1,16 +1,13 @@
 return {
   'saghen/blink.cmp',
-  dependencies = {
-    'L3MON4D3/LuaSnip',
-    version = 'v2.*',
-  },
+  dependencies = { 'rafamadriz/friendly-snippets' },
 
   version = 'v0.*',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = { preset = 'enter' },
+    keymap = { preset = 'default' },
 
     appearance = {
       use_nvim_cmp_as_default = false,
@@ -24,15 +21,18 @@ return {
       -- optionally disable cmdline completions
       -- cmdline = {},
     },
-
-    snippets = {
-      preset = 'luasnip',
-    },
-
-    fuzzy = {
-      use_frecency = true,
-      use_proximity = true,
-    },
+    --
+    -- snippets = {
+    --   preset = 'luasnip',
+    -- },
+    --
+    -- fuzzy = {
+    --   use_frecency = true,
+    --   use_proximity = true,
+    --
+    --   fuzzy = { implementation = 'prefer_rust_with_warning' },
+    -- },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
 
     -- experimental signature help support
     signature = { enabled = true },
